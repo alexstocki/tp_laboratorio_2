98d162b565b606cb418f191e4843bc70af0eccd5
Alex Stocki <alexstocki22@gmail.com>
2020-09-29 18:44:08 -0300

    Add files via upload

---
+++ Tp_n2/Entidades/Ciclomotor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Ciclomotor : Vehiculo
+    {
+        #region Propiedades
+        /// <summary>
+        /// Las motos son chicas
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Chico;
+            }
+        }
+        #endregion Propiedades
+
+        #region Constructor
+        /// <summary>
+        /// Constructor Ciclomotor. Deriva los datos al constructor de la clase base
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        public Ciclomotor(EMarca marca, string chasis, ConsoleColor color)
+            : base(chasis, marca, color)
+        {
+        }
+        #endregion Constructor
+
+        #region Metodo
+        /// <summary>
+        /// Sobrecarga del método Mostrar de la clase base
+        /// Muestra todos los datos del objeto
+        /// </summary>
+        /// <returns>string con los datos del objeto</returns>
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CICLOMOTOR");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("TAMAÑO : {0}", this.Tamanio);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+        #endregion Metodo
+
+    }
+}
